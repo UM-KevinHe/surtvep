aa3b8a2b69b738df30d0f3db1467c8eb1d5840fe
LingfengLuo0510 <lfluo@umich.edu>
2022-10-20 04:05:15 -0400

    modify

--- R/Data.R
+++ R/Data.R
@@ -13,7 +13,7 @@
 #' }
 "ExampleData"
 
-#' ExampleDataBinary
+
 #' Example data for surtvep with 2000 observations of 2 binary variables:
 #' 
 #' A simulated data set containing two 2 variables. 
@@ -29,3 +29,20 @@
 #'   \item{time}{Simulated time variable, Continous variable with non-negative value}
 #' }
 "ExampleDataBinary"
+
+
+#' Example data for stratified model illustration
+#' 
+#' A simulated data set containing two 2 variables. 
+#' @name StrataExample
+#' @docType data
+#' @usage data(StrataExample)
+#'
+#' @format A List containing the following elements:
+#' \describe{
+#'   \item{x}{Simulated covariate V1 and v2, Continuous variable. True time-dependent function is b(t)=1 and true time-dependent function is b(t)=sin(3*pi*t/4)}
+#'   \item{event}{Simulated event variable, Binary varible with 0, 1 }
+#'   \item{time}{Simulated time variable, Continous variable with non-negative value}
+#'   \item{strata}{Simulated strata variable, patients in different stratums have different baseline hazards}
+#' }
+"StrataExample"
